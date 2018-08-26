8e90680b5d6754e5320af53830ebb89bdac6aea3
Scott Ramsay <scottramsay64@gmail.com>
2018-08-26 11:44:49 +0930

    addDate.zsh: Adds .pdf support and logging of which files were renamed

--- addDate.zsh
+++ addDate.zsh
@@ -1,19 +1,24 @@
 #!/usr/bin/zsh
 
-# Assumes the year is 20xx and that the file has the .pptx extention
+# Assumes the year is 20xx and that the file has a .pptx or .pdf extention
 
 addDate() {
 	DATESTRING=$(stat "$1" | grep Modify | grep -o '20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
-	mv "$1" "$(basename -s .pptx $1) $DATESTRING.pptx"
+	mv "$1" "$(basename -s .$2 $1) $DATESTRING.$2"
+	echo "'$1' -> '$(basename -s .$2 $1) $DATESTRING.$2'" 1>&2
 }
 
 unDatedFiles() {
-	ls -1 | grep '.pptx$' | grep -v '20[0-9][0-9]-[0-9][0-9]-[0-9][0-9].pptx$'
+	ls -1 | grep ".$1$" | grep -v "20[0-9][0-9]-[0-9][0-9]-[0-9][0-9].$1$"
 }
 
-unDatedFiles | {
-while read filename
-do
-	addDate $filename
-done
+
+
+for ext in 'pdf' 'pptx'; do
+	unDatedFiles $ext | {
+	while read filename
+	do
+		addDate $filename $ext
+	done
 }
+done
