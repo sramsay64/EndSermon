c79940a3620053cd788d6005ea3ce8030f4dac0e
Scott Ramsay <scottramsay64@gmail.com>
2018-12-23 12:06:49 +1030

    Fixes bug where opening it multiple times locks audacity up

--- run-sermon.zsh
+++ run-sermon.zsh
@@ -9,9 +9,17 @@ YEARSTAMP=$(date '+%Y')
 DATESTAMP=$(date '+%Y-%m-%d-%b')
 TIMESTAMP=$(date '+%I-%M-%S-%p')
 
-echo "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
-mkdir -p "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
-cd "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
+PROCS=$(ps -a | grep 'run-sermon.z[s]h' | wc -l)
+PROCS2=$(ps -a | grep 'audacity' | wc -l)
+echo PROCS is $PROCS
+echo PROCS2 is $PROCS2
+echo 0 is $0
 
-cloneAudTemplate "Houghton-Sermon-$DATESTAMP-$TIMESTAMP"
-audacity "Houghton-Sermon-$DATESTAMP-$TIMESTAMP.aup"
+if (( $PROCS < 2 )) && (( $PROCS2 < 1 )); then
+	echo "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
+	mkdir -p "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
+	cd "/home/huc/Podcasts/$YEARSTAMP/$DATESTAMP/$TIMESTAMP/"
+	
+	cloneAudTemplate "Houghton-Sermon-$DATESTAMP-$TIMESTAMP"
+	audacity "Houghton-Sermon-$DATESTAMP-$TIMESTAMP.aup"
+fi
